02d7186d94b4f52595f6cdba61cc24c05dd8be34
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-06-15 18:05:06 -0400

    Added holes array, randHoleIdx, toggle mole, and scoreDisply.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 let score = 0;
+
+// Getting all DOM nodes containing the "hole" class and assign the list to a variable
+var holes = Array.from(document.getElementsByClassName('hole'));
+
+// Get a random # between 0 and the # of holes we have, 
+// then toggle the "mole" class in that specific <div>
+var randHoleIdx; //= Math.floor(Math.random()*(holes.length));
+// holes[randHoleIdx].classList.toggle("mole");
+
+// Repeat the code above on a timed interval using setInterval 
+function repeatMoles () {
+    randHoleIdx = Math.floor(Math.random()*(holes.length)+1);
+    holes[randHoleIdx].classList.toggle("mole");
+}
+
+setInterval(repeatMoles(), 1000);
+
+var scoreDisplay = document.getElementById('score');
+console.log(scoreDisplay);
